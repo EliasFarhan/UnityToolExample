8aba979f18be18fbaeea2f762d6f7eb0134e1acc
Elias Farhan <elias.farhan@gmail.com>
2017-11-08 10:52:52 +0100

    Tool programming

---
+++ Assets/Scripts/Background.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class Background : MonoBehaviour {
+	[SerializeField] int tileNmbX = 10;
+	[SerializeField] int tileNmbY = 10;
+
+	[SerializeField] SpriteRenderer grassPrefab;
+	Vector3 tileSize;
+
+	// Use this for initialization
+	void Start ()
+	{
+		tileSize = grassPrefab.sprite.bounds.size;
+		Generate();
+	}
+	void Clear()
+	{
+		for(int i = transform.childCount - 1; i >= 0; i--)
+		{
+			DestroyImmediate(transform.GetChild(i).gameObject);
+		}
+	}
+	public void Generate()
+	{
+		Clear();
+		for (int i = 0; i < tileNmbX; i++)
+		{
+			for (int j = 0; j < tileNmbY; j++)
+			{
+				SpriteRenderer grass = Instantiate(grassPrefab);
+				grass.transform.position = new Vector3(i, j) - new Vector3(tileNmbX / 2, tileNmbY / 2) + tileSize / 2;
+				grass.transform.parent = transform;
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/BackgroundEditor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(Background))]
+public class BackgroundEditor : Editor
+{
+
+	public override void OnInspectorGUI()
+	{
+		DrawDefaultInspector();
+		
+		Background backgroundScript = (Background)target;
+		if (GUILayout.Button("Generate Background"))
+		{
+			backgroundScript.Generate();
+		}
+	}
+}
